8b3d808f18a2dc8bd2ab396b8f5c562fdb340480
Tacus <184624026@qq.com>
2016-08-29 17:16:53 +0800

    init

--- MyMacd.py
+++ MyMacd.py
@@ -18,10 +18,12 @@ class MyMacd:
     slowModulus = 0
     deaModulus =  0
     macdDict = {}
+    deathCountDict = {}
     macdPeriod = 60
+    maxCache = 60
 
     def __init__(self,enableLog = True,macdFiled = "avg",curMacdFiled = "open",macdFastEmaModulus = 12.0,macdSlowEmaModulus = 26.0,
-        macdDEAModulus = 9.0,macdM = 2.0,macdPeriod = 60):
+        macdDEAModulus = 9.0,macdM = 2.0,macdPeriod = 60,maxCache = 60):
         self.macdFiled = macdFiled
         # 实测环境每天执行 ，handler_data 9.30调用 则只能取到昨天价格
         self.curMacdFiled = curMacdFiled
@@ -33,19 +35,75 @@ class MyMacd:
         self.fastModulus = self.macdM/self.macdFastEmaModulus+1
         self.slowModulus = self.macdM/self.macdSlowEmaModulus+1
         self.deaModulus =  self.macdM/self.macdDEAModulus+1
+        self.maxCache = maxCache
         self.util = MyUtil(enableLog)
 
-    def isMacd(self,context,data,security):
+    def isMacdGoldCross(self,context,data,security):
+        
+        self.calculateMacdInfo(context,data,security)
+        list = self.getLastTradeDayMacd(security,3)
+        if(list and len(list)>2):
+            prePreInfo = list[0]
+            preInfo = list[1]
+            curInfo = list[2]
+            self.util.logPrint("isMacdGoldCross,curInfo:%s,preInfo:%s,dea:%s"
+                            , curInfo["macd"],preInfo["macd"],curInfo["dea"])
+
+            if(preInfo["macd"] <0 and curInfo["macd"] >0)
+             or (prePreInfo["macd"]<=0 and preInfo["macd"] = 0 and curInfo["macd"] >0):
+                self.util.logPrint ('金叉')
+                return True  
+        return False
+
+    def isMacdLowGoldCross(self,context,data,security):
+        # 低位金叉
+        self.calculateMacdInfo(context,data,security)
+        list = self.getLastTradeDayMacd(security,3)
+        if(list and len(list)>2):
+            prePreInfo = list[0]
+            preInfo = list[1]
+            curInfo = list[2]
+            self.util.logPrint("isMacdGoldCross,curInfo:%s,preInfo:%s,dea:%s"
+                            , curInfo["macd"],preInfo["macd"],curInfo["dea"])
+
+            if(preInfo["macd"] <0 and curInfo["macd"] >0 and curInfo["dea"]<0)
+             or (prePreInfo["macd"]<=0 and preInfo["macd"] = 0 and curInfo["macd"] >0 and curInfo["dea"]<0):
+                self.util.logPrint ('金叉')
+                return True  
+        return False
+        
+    def isMacdHightGoldCross(self,context,data,security):
+        pass
+
+    def isMacdLowDeathCross(self,context,data,security):
+        pass
+
+    def isMacdHightDeathCross(self,context,data,security):
+        #高位死叉
+        self.calculateMacdInfo(context,data,security)
+        list = self.getLastTradeDayMacd(security,3)
+        if(list and len(list)>2):
+            prePreInfo = list[0]
+            preInfo = list[1]
+            curInfo = list[2]
+            self.util.logPrint("isMacdGoldCross,curInfo:%s,preInfo:%s,dea:%s"
+                            , curInfo["macd"],preInfo["macd"],curInfo["dea"])
+
+            if(preInfo["macd"] >0 and curInfo["macd"] < 0 and curInfo["dea"]>0)
+             or (prePreInfo["macd"]>=0 and preInfo["macd"] = 0 and curInfo["macd"] < 0 and curInfo["dea"]>0):
+                self.util.logPrint ('死叉')
+                return True  
+        return False
+
+    def calculateMacdInfo(self,context,data,security):
         dict = data[security]
+        factor = 1.0
         if(not dict.isnan() and not dict.paused):
             curPrice = dict.close
             factor =  dict.factor 
-            return self.recursionCalMacd(context,security,curPrice,factor)
-        
-        return False
-        
-    #当前停牌 当前退市 当前未上市
-    def recursionCalMacd(self,context,security,curPrice,factor):
+        else:
+            return None
+
         if(not factor == 1.0):
             self.removeTradeDayMacd(security)   
             self.util.logPrint ("除权factor:%s，重新计算macd",factor)
@@ -65,24 +123,28 @@ class MyMacd:
             lastFastEma = macdDict['fastEma']
             lastDea = macdDict['dea']
             timeDt = context.current_dt.date() - lastDate
-            if(timeDt.days > 1):
+            if(timeDt.days > 0):
                 start_date = lastDate
-                end_date = context.current_dt.date() - timedelta(1)
+                end_date = context.current_dt.date()
                 df = get_price(security, start_date = start_date ,end_date=end_date, fields=[self.macdFiled], skip_paused=True)
                 avgs = df[self.macdFiled]
                 for i in range(len(avgs)):
                      # self.util.logPrint ("lastSlowEma:%s,lastFastEma:%s,lastDea:%s" ,lastSlowEma,lastFastEma,lastDea)
+                    date = avgs.index[i].date()
                     curHisPrice = avgs[i]
                     currentSlowEma,currentFastEma,currentDea,currentMacd,currentDiff = self.caculateMacd(lastSlowEma,lastFastEma,lastDea,curHisPrice)
+                    self.setLastTradeDayMacd(security,currentSlowEma,currentFastEma,currentDea,currentMacd,date)
                     lastSlowEma = currentSlowEma
                     lastFastEma = currentFastEma
                     lastDea = currentDea
                     lastMacd = currentMacd
-                    date = avgs.index[i].date()
                     # self.util.logPrint ("中继date:%s, currentMacd:%s,currentDiff:%s,currentDea:%s" ,str(avgs.index[i].date()),currentMacd,currentDiff,currentDea)
-                 
+            # #当前已经计算过
+            # else:
+            #     return
+            
         else:
-            end_date = context.current_dt.date() - timedelta(1)
+            end_date = context.current_dt.date()
             dict = get_security_info(security)
             startDate = dict.start_date
             days = end_date - startDate 
@@ -111,25 +173,20 @@ class MyMacd:
                 lastFastEma = currentFastEma
                 lastDea = currentDea
                 lastMacd = currentMacd
+                date = avgs.index[i].date()
                 # self.util.logPrint ("date:%s, currentMacd:%s,currentDiff:%s,currentDea:%s" ,str(avgs.index[i].date()),currentMacd,currentDiff,currentDea)
-       
-        currentSlowEma,currentFastEma,currentDea,currentMacd,currentDiff = self.caculateMacd(lastSlowEma,lastFastEma,lastDea,curPrice)
-        self.setLastTradeDayMacd(security,currentSlowEma,currentFastEma,currentDea,context.current_dt.date())
-        # caculateMacd(lastSlowEma,lastFastEma,lastDea,curPrice)
-        # record(price = curPrice)
-        # record(macd=currentMacd,diff = currentDiff,dea = currentDea,stand = 0)
-        
-        #低位金叉
-        #self.util.logPrint lastMacd, currentMacd,
-        if(lastMacd <0 and currentMacd >=0):
-            self.util.logPrint ('金叉')
-            return True
-            
-        if(lastMacd >0 and currentMacd <=0):
-            self.util.logPrint ('死叉')
-        #高位金叉
-        
-        return False
+                self.setLastTradeDayMacd(security,currentSlowEma,currentFastEma,currentDea,currentMacd,date)
+        # currentSlowEma,currentFastEma,currentDea,currentMacd,currentDiff = self.caculateMacd(lastSlowEma,lastFastEma,lastDea,curPrice)
+        # self.setLastTradeDayMacd(security,currentSlowEma,currentFastEma,currentDea,currentMacd,context.current_dt.date())
+        # # caculateMacd(lastSlowEma,lastFastEma,lastDea,curPrice)
+        # # record(price = curPrice)
+        # # record(macd=currentMacd,diff = currentDiff,dea = currentDea,stand = 0)
+        # info = {}
+        # info["currentMacd"] = currentMacd
+        # info["lastMacd"] = lastMacd
+        # info["currentDiff"] = currentDiff
+        # info["currentDea"] = currentDea
+        # return info        
         
     def caculateMacd(self,lastSlowEma,lastFastEma,lastDea,curPrice):
         currentSlowEma = lastSlowEma*(1-self.slowModulus) + curPrice*self.slowModulus
@@ -140,35 +197,61 @@ class MyMacd:
         return currentSlowEma,currentFastEma,currentDea,currentMacd,currentDiff
         
         
-    def setLastTradeDayMacd(self,security,slowEma,fastEma,Dea,date):
+    def setLastTradeDayMacd(self,security,slowEma,fastEma,Dea,Macd,date):
         if self.macdDict.has_key(security):
-            securityDt = self.macdDict[security]
-            # macdDict = {}
-            # macdDict["slowEma"] = slowEma
-            # macdDict["fastEma"] = fastEma
-            # macdDict["dea"] = Dea
-            # macdDict["date"] = date
-            # securityDt.append(macdDict)
-            macdDict = securityDt[len(securityDt) - 1]
-            macdDict["slowEma"] = slowEma
-            macdDict["fastEma"] = fastEma
-            macdDict["dea"] = Dea
-            macdDict["date"] = date
-        else:
-            securityDt = list()
+            securityList = self.macdDict[security]
+            if(len(securityList) >= self.maxCache):
+                macdDict = del securityList[0]
             macdDict = {}
             macdDict["slowEma"] = slowEma
             macdDict["fastEma"] = fastEma
             macdDict["dea"] = Dea
             macdDict["date"] = date
-            securityDt.append(macdDict)
-            self.macdDict[security] = securityDt
+            macdDict["macd"] = Macd           
+            securityList.append(macdDict)
+        else:
+            securityList = list()
+            macdDict = {}
+            macdDict["slowEma"] = slowEma
+            macdDict["fastEma"] = fastEma
+            macdDict["dea"] = Dea
+            macdDict["date"] = date
+            macdDict["macd"] = Macd            
+            securityList.append(macdDict)
+            self.macdDict[security] = securityList
     def removeTradeDayMacd(self,security):
         if self.macdDict.has_key(security):
            del self.macdDict[security]
             
-    def getLastTradeDayMacd(self,security):
+    def getLastTradeDayMacd(self,security,count = None):
         if self.macdDict.has_key(security):
-            securityDt = self.macdDict[security]
-            macdDict = securityDt[len(securityDt) - 1]
-            return macdDict
+            securityList = self.macdDict[security]
+            ret
+            len = len(securityList)
+            if(count and len>=count):
+                ret = securityList[len - count:]
+            elif count:
+                ret = securityList
+            else:
+                ret = securityList[len(securityList) - 1]
+            return ret
+
+    #return list
+    def getAllTradeDayMacd(self,security):
+        if self.macdDict.has_key(security):
+            securityList = self.macdDict[security]
+            return securityList
+
+
+    def getCurDeathCount(self,security):
+        iRet = 0
+        if self.deathCountDict.has_key(security):
+            iRet = self.deathCountDict[security]
+        return iRet
+
+    def setCurDeathCount(self,security,count):
+        self.deathCountDict[security] = count
+
+    def removeCurDeathCount(self,security):
+        if self.deathCountDict.has_key(security):
+            del self.deathCountDict[security]
---
+++ volumeRation2.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+# import matplotlib as talib
+import talib
+import datetime
+
+def initialize(context):
+    set_option('use_real_price', True)
+    g.security = get_all_securities(["stock"]).index
+
+    #较长时间周期
+    g.longPeriod = 45.0
+
+    #较短时间周期
+    g.shortPeriod = 2.0
+
+    #倍率
+    g.ratio = 2.0
+
+def handle_data(context, data):
+    securities = g.security
+    for security in securities:
+        dict = data[security]
+        if(not dict.isnan() and not dict.paused):            
+            volume_short = get_price(security,end_date = context.current_dt,frequency ='1d', fields = ['volume'],count = 2,skip_paused=True)
+            volume_short_per = volume_short['volume'].sum()/g.shortPeriod
+            volume_long = get_price(security,end_date = context.current_dt,frequency ='1d', fields = ['volume'],count = 45,skip_paused=True)
+            volume_long_per = volume_long['volume'].sum()/g.longPeriod
+            ratio = volume_short_per/volume_long_per
+            if(ratio >= g.ratio):
+                print "Code:%s,ratio：%s ,two days volume：%s,ForyFive days volume：%s"%(security,ratio,volume_short_per,volume_long_per)
