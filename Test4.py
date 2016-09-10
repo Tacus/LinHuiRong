a30368a06b1121fe3df4f022657bde6833874105
Tacus <184624026@qq.com>
2016-09-10 16:51:51 +0800

    sdfsd

--- Test4.py
+++ Test4.py
@@ -1,18 +1,18 @@
 get_money_flow
 
 get_money_flow(security_list, start_date, end_date, fields=None)
-获取一只或者多只股票在一个时间段内的资金流向数据
+获取一只或者多只在一个时间段内的资金流向数据
 
 参数
 
-security_list: 一只股票代码或者一个股票代码的 list
+security_list: 一只代码或者一个代码的 list
 start_date: 开始日期, 一个字符串或者 datetime.datetime/datetime.date 对象
 end_date: 结束日期, 一个字符串或者 datetime.date/datetime.datetime 对象
 fields: 字段名或者 list, 可选. 默认为 None, 表示取全部字段, 各字段含义如下：
 
 字段名	含义	备注
 date	日期	
-sec_code	股票代码	
+sec_code	代码	
 change_pct	涨跌幅(%)	
 net_amount_main	主力净额(万)	主力净额 = 超大单净额 + 大单净额
 net_pct_main	主力净占比(%)	主力净占比 = 主力净额 / 成交额
@@ -30,12 +30,12 @@ net_pct_s	小单净占比(%)	小单净占比 = 小单净额 / 成交额
 
 示例
 
-# 获取一只股票在一个时间段内的资金流量数据
+# 获取一只在一个时间段内的资金流量数据
 get_money_flow('000001.XSHE', '2016-02-01', '2016-02-04')
 get_money_flow('000001.XSHE', '2015-10-01', '2015-12-30', fields="change_pct")
 get_money_flow(['000001.XSHE'], '2010-01-01', '2010-01-30', ["date", "sec_code", "change_pct", "net_amount_main", "net_pct_l", "net_amount_m"])
 
-# 获取多只股票在一个时间段内的资金流向数据
+# 获取多只在一个时间段内的资金流向数据
 get_money_flow(['000001.XSHE', '000040.XSHE', '000099.XSHE'], '2010-01-01', '2010-01-30')
-# 获取多只股票在某一天的资金流向数据
+# 获取多只在某一天的资金流向数据
 get_money_flow(['000001.XSHE', '000040.XSHE', '000099.XSHE'], '2016-04-01', '2016-04-01')
