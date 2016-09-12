2288a72a1051bc29aa61ce65bc329cb4e17fee34
DESKTOP-0RTG43B\国兵哥哥 <184624026@qq.com>
2016-09-12 23:51:48 +0800

    Signed-off-by: DESKTOP-0RTG43B\国兵哥哥 <184624026@qq.com>

--- Test5.py
+++ Test5.py
@@ -1,4 +1,6 @@
-a = float('3')
-b = float(23)
-
-print a * b
+ ['__add__', '__class__', '__delattr__', '__doc__', '__eq__', '__format__',
+  '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+   '__ne__', '__new__', '__radd__', '__reduce__', '__reduce_ex__', '__repr__', '__rsub__', 
+   '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', 'ctime', 'day', 
+   'fromordinal', 'fromtimestamp', 'isocalendar', 'isoformat', 'isoweekday', 'max', 'min', 'month',
+  'replace', 'resolution', 'strftime', 'timetuple', 'today', 'toordinal', 'weekday', 'year']
