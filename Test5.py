c2427b0063de65d1d7914dc1100d35b9d8e9dbae
Tacus <184624026@qq.com>
2016-09-18 09:33:06 +0800

    sdf

--- Test5.py
+++ Test5.py
@@ -1,4 +1,32 @@
-a = float('3')
-b = float(23)
+# import sys
+import os
 
-print a * b
+# print sys.path
+# print dir(sys)
+# print sys.prefix
+# python -c"import sys;print
+
+
+# os.environ['PATH'] += os.pathsep + '/usr/local/bin'
+
+cmd = "protoc --lua_out=" + "" + " --proto_path=" + ""+" "+""
+os.system(cmd)
+
+# print os.environ
+
+# '_': '/usr/bin/python', 
+# 'PYTHONIOENCODING': 'utf-8', 
+# 'Apple_PubSub_Socket_Render': '/private/tmp/com.apple.launchd.liYsXla2M4/Render', 
+# 'VERSIONER_PYTHON_PREFER_32_BIT': 'no', 'VERSIONER_PYTHON_VERSION': '2.7', 'SHLVL': '1', 
+# 'SSH_AUTH_SOCK': '/private/tmp/com.apple.launchd.5G27FpKrER/Listeners', 
+# 'SECURITYSESSIONID': '186a8', 'XPC_FLAGS': '0x0', '__CF_USER_TEXT_ENCODING': '0x1F5:0x19:0x34', 
+# 'PWD': '/Users/tacus/develop/workspace_python/LinHuiRong', 
+# 'SHELL': '/bin/bash',
+# 'LOGNAME': 'tacus', 
+# 'USER': 'tacus', 
+# 'XPC_SERVICE_NAME': 'com.apple.xpc.launchd.oneshot.0x10000007.Sublime Text', 
+# 'HOME': '/Users/tacus', 
+# 'PATH': '/Library/Frameworks/Python.framework/Versions/3.5/bin:/Users/tacus/develop/android-sdk-macosx/tools:/Users/tacus/develop/android-sdk-macosx/platform-tools:/usr/local/share/autoconf/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Frameworks/Python.framework/Versions/3.5/bin:/Users/tacus/develop/android-sdk-macosx/tools:/Users/tacus/develop/android-sdk-macosx/platform-tools:/usr/local/share/autoconf/bin', 
+# 'NDK_ROOT': '/Users/tacus/develop/android-ndk-r10d',
+# 'TMPDIR': '/var/folders/0y/csyrvhts6wgfkp8jq35jbxgm0000gn/T/', 
+# 'COMMAND_MODE': 'unix2003'
