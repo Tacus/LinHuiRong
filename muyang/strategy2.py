0fa59d02002a76ebc6b424f06d7491f41ddf6d7a
zhangguobing <zhangguobing@xindong.com>
2018-10-26 09:39:40 +0800

    xxx

--- muyang/strategy2.py
+++ muyang/strategy2.py
@@ -14,9 +14,6 @@ def initialize(context):
     # 开启动态复权模式(真实价格)
     set_option('use_real_price', True)
 
-#注意申万指数在2014年有一次大改,聚源使用的是为改变之前的代码,官网包含更改前和更改后的代码,如果遇到找不到的标的可以根据需求自行查找
-#如801124 >>801121食品加工II
-
 
 def get_sw_quote(codes,end_date=None,count=None,start_date=None):
     '''获取申万指数行情,返回panel结构'''
@@ -37,19 +34,21 @@ def get_sw_quote(codes,end_date=None,count=None,start_date=None):
     return df2.to_panel()
     
     #做排序
-def get_sw_industry_stocks(name,data,datetime,count):
+def get_sw_industry_stocks(name,datetime,count):
     codes = get_industries(name).index;
     panel_industry = get_sw_quote(codes,end_date=datetime,count=count)
     industry_closes = panel_industry.ClosePrice
+    get_market_closes(datetime,count)
     for i in range(len(codes)):
         industry_code = codes[i]
         if(not industry_code in industry_closes.columns):
             continue
         industry_close = industry_closes[industry_code]
         securities = get_industry_stocks(industry_code)
-        panel = get_price(security = securities,end_date = datetime,count = count,fields = ['pre_close','close'])
+        panel = get_price(security = securities,end_date = datetime,count = count,fields = ['pre_close','close','volume'])
         df_preclose = panel["pre_close"]
         df_close = panel["close"]
+        df_volume = panel["volume"]
         df = (df_close - df_preclose)/df_preclose
         stock_infos = list()
         for security in securities:
@@ -57,14 +56,16 @@ def get_sw_industry_stocks(name,data,datetime,count):
             stock_close = df_close[security]
             series_rs = stock_close/industry_close
             ema_rs = tb.EMA(np.array(series_rs),39)
+            volume = df_volume[security][-1]
             cur_rs = series_rs[-1]
-
-            stock_info = StockInfo(security,industry_code,name)
-            stock_info.init_data(increase,cur_rs,ema_rs)
+            close_price = stock_close[-1]
+            stock_info = StockInfo(security,close_price,industry_code,name)
+            stock_info.init_data(increase,cur_rs,ema_rs,volume)
             stock_infos.append(stock_info)
             # stock_info
         stock_infos = sorted(stock_infos,key = lambda data: data.increase,reverse = True)
         pick_count = 0
+        new_industry = CustomIndustry(industry_code)
         for stock_info in stock_infos:
             cur_rs = stock_info.cur_rs
             ema_rs = stock_info.ema_rs
@@ -72,9 +73,14 @@ def get_sw_industry_stocks(name,data,datetime,count):
                 break
             if(cur_rs>ema_rs):
                 pick_count+=1
-                print(stock_info)
+                new_industry.add_stockinfo(stock_info)
+
 def initialize(context):
     run_daily(get_lastday_increase)
+
+def get_market_closes(datetime,count):
+    codes = ["000001.XSHG","399006.XSHE","399005.XSHE"]
+    get_price(codes,end_date = datetime,count = count,filter = ["close"])
 
 def get_lastday_increase(context):
     print("get_lastday_increase",context.current_dt)
@@ -83,8 +89,8 @@ def get_lastday_increase(context):
 def handle_data(context,data):
     current_dt = context.current_dt
     end_date = current_dt - timedelta(days = 1)
-    get_sw_industry_stocks("sw_l1",data,end_date,60)
-    get_sw_industry_stocks("sw_l2",data,end_date,60)
+    get_sw_industry_stocks("sw_l1",end_date,60)
+    get_sw_industry_stocks("sw_l2",end_date,60)
 
 class StockInfo:
     def __init__(self,security,industry,swl):
@@ -92,10 +98,29 @@ class StockInfo:
         self.industry = industry
         self.swl = swl
         pass
-    def init_data(self,increase,cur_rs,ema_rs):
+    def init_data(self,increase,close_price,cur_rs,ema_rs,volume):
         self.increase = increase
+        self.close_price = close_price
         self.cur_rs = cur_rs
         self.ema_rs = ema_rs[-1]
+        self.volume = volume
     def __str__(self):
         value = "name:%s,increase:%s,cur_rs:%s,ema_rs:%s"%(self.security,self.increase,self.cur_rs,self.ema_rs)
         return value
+class CustomIndustry:
+    def __init__(self,industry):
+        self.industry = industry
+        self.stock_infos = list()
+        self.value = 0
+    def init_data(self):
+        pass
+    def add_stockinfo(self,stock_info):
+        self.stock_infos.append(stock_info)
+    def get_value(self):
+        if(self.value != 0):
+            return self.value
+        for stock_info in self.stock_infos:
+            self.value += stock_info.close_price
+        self.value = round(self.value/len(self.stock_infos),2)
+    def __str__(self):
+        return "CustomIndustry"
