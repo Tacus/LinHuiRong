9d3550285591aa9eab597a2544281f1438067dbd
zhangguobing <zhangguobing@xindong.com>
2018-11-28 20:28:01 +0800

    xxx

--- study_matlib/test_matlib.py
+++ study_matlib/test_matlib.py
@@ -1,25 +1,19 @@
 # coding=utf-8
-##先导入各种库
 import datetime
 import time
 import pandas as pd
-import talib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdate
-import matplotlib.finance as fin
+# import matplotlib.finance as fin
 import numpy as np
 
-x = np.linspace(0, 5, 10)
-# x = np.linspace(0, 5, 10,False)
-y = x ** 2
-y = np.zeros(10)
+x,step = np.linspace(0,5,10,True,True)
 
-
-plt.figure()
-plt.plot(x,y,'r--')
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("hahah")
+ax1=plt.subplot2grid((4,1),(0,0),rowspan=3)
+ax2 = plt.subplot2grid((4,1),(3,0),sharex = ax1)
+series = pd.Series(x)
+series.plot(ax = ax1,legend=True,style = "r--",)
+print(series)
 plt.show()
 
 # plt.subplot(1,2,1)
