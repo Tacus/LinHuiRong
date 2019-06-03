1ca85b3f04d527af5414fe59acb0f98f983163ec
zhang guobing <184624026@qq.com>
2019-06-04 00:00:08 +0800

    xxx

--- autogit.py
+++ autogit.py
@@ -4,3 +4,4 @@ os.system("git pull")
 os.system("git add .")
 os.system("git commit -m \"xxx\"")
 os.system("git push")
+
--- muyang/test_1.py
+++ muyang/test_1.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import numpy as np
-df=pd.DataFrame(np.random.rand(4,4), index=list('abcd'), columns=list('ABCD'))
-# print(dir(df))
-print('A' in df.columns)
+a =  []
+
+print(a.count(1))
+
+
